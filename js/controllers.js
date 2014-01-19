6261d623642a58096047b33fcae08430d8ebb352
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-01-18 23:35:25 -0700

    Add date and time to Index

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,33 @@
 angular.module('mini-data.controllers', [])
   .controller('mainCtrl', function($scope){
-    $scope.test = 'teesfefst';
+    
+    var getTimeInfo = function(){
+      var now = new Date();
+      var hours = now.getHours();
+      var period = 'AM';
+      var minutes = now.getMinutes();
+      if(hours > 12){
+        hours -= 12;
+        period = 'PM';
+      }
+      minutes < 10 && (minutes = '0' + minutes);
+      return hours + ':' + minutes + period; 
+    }
+
+    var getDateInfo = function(){
+      var monthObj = {
+        0: 'January', 1: 'February', 2: 'March', 3: 'April', 4: 'May', 5: 'June',
+        6: 'July', 7: 'August', 8: 'September', 9: 'October', 10: 'November', 11: 'December'
+      }
+
+      var now = new Date();
+      var day = now.getDate();
+      var month = monthObj[now.getMonth()];
+      var year = now.getFullYear();
+
+      return month + ' ' + day + 'th, ' + year;
+    }
+
+    $scope.time = getTimeInfo();
+    $scope.date = getDateInfo();
   });
