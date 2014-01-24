5396097efb8b96a2c35da39dbf5754eb39e9b35a
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-01-23 21:05:46 -0700

    Add basic structure to Stats page, submit guard on goals page, more styling

--- js/goals-data-controller.js
+++ js/goals-data-controller.js
@@ -1,4 +1,25 @@
 angular.module('mini-data.controllers')
   .controller('goalsDataCtrl', function($scope){
-    
+    $scope.dummyData = [
+      {
+        goal: 'lose 5 pounds',
+        time: 'once',
+        every: 'week'
+      },
+      {
+        goal: 'Lose 5 Pounds',
+        time: 'once',
+        every: 'week'
+      },
+      {
+        goal: 'Lose 5 Pounds',
+        time: 'once',
+        every: 'week'
+      },
+      {
+        goal: 'Lose 5 Pounds',
+        time: 'once',
+        every: 'week'
+      }
+    ]
   });
--- js/new-goal-controller.js
+++ js/new-goal-controller.js
@@ -1,4 +1,15 @@
 angular.module('mini-data.controllers')
   .controller('newGoalCtrl', function($scope){
+    $scope.goal = {
+      text: '',
+      time: 'once',
+      every: 'day'
+    }
     
+    $scope.submitGoal = function(){
+      if(!$scope.goal.text) return;
+      $scope.goal.text = $scope.goal.text.toLowerCase();
+      //submit $scope.goal
+      $scope.showGoalsFn();
+    }
   });
