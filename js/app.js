6ef6f987ae376a0f9c17bffd723bf0e593924314
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-02-16 20:32:09 -0700

    Add login/logout fb functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 angular.module('mini-data.controllers', []);
 angular.module('mini-data.services', []);
-angular.module('mini-data', ['ngAnimate', 'angularCharts', 'mini-data.controllers', 'mini-data.services']);
+angular.module('mini-data', ['ngAnimate', 'angularCharts', 'ui.bootstrap', 'mini-data.controllers', 'mini-data.services']);
